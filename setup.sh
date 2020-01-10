ebb71a6385e110444603e1eb83b5081ec5130320
cchudant <skybt@pm.me>
2020-01-10 14:08:56 +0100

    (nearly) everything now on alpine

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 eval $(minikube docker-env)
-docker build -t custom-nginx:1.1 srcs/nginx
+docker build -t custom-nginx:1.11 srcs/nginx
+docker build -t custom-ftps:1.2 srcs/ftps
+docker build -t custom-wordpress:1.7 srcs/wordpress
+docker build -t custom-phpmyadmin:1.1 srcs/phpmyadmin
 
 if [ "$1" = "delete" ]
 then
