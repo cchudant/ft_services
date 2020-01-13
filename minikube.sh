b9a3c5a7e338572c558186bf9bb669d8659f9015
cchudant <skybt@pm.me>
2020-01-13 21:51:22 +0100

    pretty setup.sh

--- minikube.sh
+++
@@ -1,3 +0,0 @@
-minikube start --vm-driver=virtualbox --cpus 3 --disk-size=30000mb --memory=3000mb
-minikube addons enable metrics-server
-minikube addons enable ingress
--- setup.sh
+++ setup.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
+if ! which docker >/dev/null 2>&1 ||
+    ! which minikube >/dev/null 2>&1;
+then
+    echo Please install Docker and Minikube
+    exit 1
+fi
+
+if ! minikube status >/dev/null 2>&1
+then
+    echo Minikube is not started! Starting now...
+    if ! minikube start --vm-driver=virtualbox \
+        --cpus 3 --disk-size=30000mb --memory=3000mb
+    then
+        echo Cannot start minikube!
+        exit 1
+    fi
+    minikube addons enable metrics-server
+    minikube addons enable ingress
+fi
+
 eval $(minikube docker-env)
 docker build -t custom-nginx:1.11 srcs/nginx
 docker build -t custom-ftps:1.2 srcs/ftps
